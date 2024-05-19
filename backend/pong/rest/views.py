f98e2aec32f467137b92a55540f1354cf262cff0
Brahim Amrouch <amroucheb4@gmail.com>
2024-05-19 17:38:27 +0100

    First Api Endpoint Working

--- backend/pong/rest/views.py
+++ backend/pong/rest/views.py
@@ -1,27 +1,48 @@
-from django.http import HttpResponse
-from rest_framework.views import APIView
+import uuid
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+from rest_framework.decorators import action
+from rest_framework import status
+from .models import User
+from .serializers import UserSerializer
 
-def index(request):
-    return HttpResponse("Hello there From Index Starting")
-
-class UserInfo(APIView):
+class UserInfo(ViewSet):
     """
         Get a Specific User Info
     """
-    def get(self, request, id):
-        return None
+    def fetch_user(self, id):
+        try:
+            user_id = None
+            if not isinstance(id, uuid.UUID):
+                user_id = uuid.UUID(id)
+            user = User.objects.get(id=user_id)
+            return user
+        except User.DoesNotExist:
+            return None
+
+    @action(['get'], True)
+    def get_user(self, request, id=None):
+        user = self.fetch_user(id)
+        if user == None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serialized_user = UserSerializer(user).data
+        return Response(serialized_user, status=status.HTTP_200_OK)
 
     """
         Create New User,
         Modify A Specific User Info
     """
-    def post(self, request, id=-1):
-        return None
+    @action(['post'], True)
+    def create_user(self, request):
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
     
     """
         Delete Specific User
     """
-    def delete(self, request, id):
-        return None
+    @action(['delete'], True)
+    def delete_user(self, request, id):
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+    # def delete_user(self, request, id):
+    #     return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 # Create your views here.
