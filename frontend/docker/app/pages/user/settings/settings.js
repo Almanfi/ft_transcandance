09fc77df0eeaeac089426be1ab41c1b6d4fbd3ba
Mohammed Hrima <mhrima@e2r5p1.1337.ma>
2024-12-10 20:11:01 +0100

    update

--- frontend/docker/app/pages/user/settings/settings.js
+++
@@ -1,9 +0,0 @@
-import Ura from 'ura';
-function Settings(props = {}) {
-    const [render, State] = Ura.init();
-    const [getter, setter] = State(0);
-    return render(() => (Ura.e("div", { className: "settings" },
-        Ura.e("div", { className: "card" },
-            Ura.e("img", { src: "", alt: "" })))));
-}
-export default Settings;
