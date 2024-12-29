29e335a558b8e0cc98a8263a8b2234e1786604b3
Almanfi <nassimed.dev@gmail.com>
2024-12-29 23:47:07 +0100

    start refactoring

--- backend/pong/shooter/static/osnierTS/assets.ts
+++ backend/pong/shooter/static/osnierTS/assets.ts
@@ -1,44 +1,88 @@
 import * as THREE from 'three';
-export function initPlane() {
-    const geometry = new THREE.PlaneGeometry(400, 300);
-    const material = new THREE.MeshBasicMaterial({ color: 0xcccccd, side: THREE.DoubleSide });
-    const plane = new THREE.Mesh(geometry, material);
+import { Move, GameAction } from './keyControls';
+
+
+export function initPlane(): THREE.Mesh {
+    const geometry = new THREE.PlaneGeometry( 400, 300 );
+    const material = new THREE.MeshBasicMaterial( {color: 0xcccccd, side: THREE.DoubleSide} );
+    const plane = new THREE.Mesh( geometry, material );
     plane.receiveShadow = true;
     plane.position.set(0, -3, 0);
     plane.rotateX(Math.PI / 2);
+
     return plane;
 }
+
+
 class Inputs {
+    order: number
+    move: Move
+    angle: number
+    direction: THREE.Vector3
+    action: GameAction
+    timeStamp: number
+
     constructor() {
         this.order = 0;
-        this.move = { x: 0, y: 0 };
+        this.move = {x: 0, y: 0};
         this.angle = 0;
         this.direction = new THREE.Vector3(1, 0, 0);
-        this.action = { f: false, d: false };
+        this.action = {f: false, d: false};
         this.timeStamp = 0;
     }
-    calcMovementVector(frontVector) {
+
+    calcMovementVector(frontVector: THREE.Vector3): THREE.Vector3 {
         var speedVect = new THREE.Vector3(0, 0, 0);
         const sideOnPlane = frontVector.clone().cross(new THREE.Vector3(0, 1, 0));
         let move = this.move;
-        speedVect.addScaledVector(frontVector, move.y);
-        speedVect.addScaledVector(sideOnPlane, move.x);
+    
+        speedVect.addScaledVector(frontVector, move.x);
+        speedVect.addScaledVector(sideOnPlane, move.y);
         speedVect.normalize();
         return speedVect;
     }
+
     fire() {
         return this.action.f;
     }
-    set(move, angle, direction, action, timeStamp) {
+
+    set(move: Move, angle: number, direction: THREE.Vector3,
+        action: GameAction, timeStamp: number) {
         this.order++;
-        this.move = { x: move.x, y: move.y };
+        this.move = {x: move.x, y: move.y};
         this.angle = angle;
         this.direction = direction.clone();
-        this.action = { f: action.f, d: action.d };
+        this.action = {f: action.f, d: action.d};
         this.timeStamp = timeStamp;
     }
 }
+
+
 export class Player extends THREE.Object3D {
+    radius: number;
+    core: THREE.Mesh;
+    cannon: CannonObject;
+
+    fireRate: number;
+    bulletDelay: number;
+    lastFire: number;
+    fired: boolean;
+
+    inputs: Inputs;
+    actions: Map<number, Inputs>;
+
+    oldPosition: THREE.Vector3;
+    movementVector: THREE.Vector3;
+
+    scene: THREE.Scene;
+    planeFacingVector: THREE.Vector3;
+
+    // uncomment for bullet sound
+    // bulletSound: THREE.Audio;
+    bulletManager: any;
+
+
+
     constructor(position) {
         super();
         this.radius = 1.5;
@@ -48,39 +92,49 @@ export class Player extends THREE.Object3D {
         this.cannon = cannon;
         this.add(core);
         this.add(cannon);
+
         this.position.set(position.x, position.y, position.z);
         let scale = 2;
         this.scale.set(scale, scale, scale);
+
         this.fireRate = 100;
         this.bulletDelay = 50;
         this.lastFire = 0;
         this.fired = false;
+
         this.inputs = new Inputs();
-        this.oldPosition = this.position.clone();
+
+        this.oldPosition =  this.position.clone();
         this.movementVector = new THREE.Vector3();
         this.actions = new Map();
-    }
-    ;
+    };
+
     reset() {
         this.actions.clear();
         this.lastFire = 0;
     }
-    addToScene(scene) {
+
+    addToScene(scene: THREE.Scene) {
         this.scene = scene;
-        scene.add(this);
-    }
-    setPlaneVector(planeFacingVector) {
+		scene.add(this);
+	}
+
+    setPlaneVector(planeFacingVector: THREE.Vector3) {
         this.planeFacingVector = planeFacingVector;
     }
+
     // attachControls(controls) {
     //     this.controls = controls;
     // }
-    addBulletSound(sound) {
+
+    addBulletSound(sound: THREE.Audio) {
         // this.bulletSound = sound;
     }
+
     addBulletManager(bulletManager) {
         this.bulletManager = bulletManager;
     }
+
     despawnUncertainBullets(time) {
         time = time - this.bulletDelay;
         // console.log('despawning bullets after time: ', time);
@@ -97,6 +151,7 @@ export class Player extends THREE.Object3D {
             }
         });
     }
+
     rollbackActoin(action, lastActionTime, planeFacingVector, actionTime) {
         // let spanS = (action.timeStamp - lastActionTime);
         // console.log('rolling back action old actionat time: ', lastActionTime, " to: ", action.timeStamp);
@@ -105,6 +160,7 @@ export class Player extends THREE.Object3D {
         // console.log(`at time of new action: ${action.timeStamp}: position: ${JSON.stringify(this.position)}`);
         // this.controls.applyAction(action);
     }
+
     rollBack(timeStamp) {
         // this.position.copy(this.controls.position);
         // this.oldPosition.copy(this.position);
@@ -117,18 +173,23 @@ export class Player extends THREE.Object3D {
         // }
         // this.addRollBackAction(timeStamp);
     }
+    
     update(timeS, timeStamp, actionTime) {
         let speed = (30 * timeS) / 1000;
         const projectionOnPlane = this.planeFacingVector;
+        
         this.core.rotateY(0.1);
         // this.cannon.head.rotateOnAxis(new THREE.Vector3(0, 1, 0), 0.05);
-        this.cannon.setRotationFromAxisAngle(new THREE.Vector3(0, 1, 0), this.inputs.angle);
+        this.cannon.setRotationFromAxisAngle(new THREE.Vector3(0,1,0), this.inputs.angle);
+        
         this.oldPosition.copy(this.position);
         // this.oldMovementVector.copy(this.movementVector);
         this.movementVector.copy(this.inputs.calcMovementVector(projectionOnPlane));
         // console.log(`movement vector: of ${this.name}`, this.movementVector);
         // this.movementVector.multiplyScalar(speed);
+
         this.position.addScaledVector(this.movementVector, speed);
+
         // if (this.controls.sendActionToPeer && this.saveAction) {
         //     this.currInput = {};
         //     this.currmouse = {};
@@ -146,12 +207,14 @@ export class Player extends THREE.Object3D {
         //     Object.assign(this.move, this.currInput);
         // }
         // this.addAction(actionTime);
+
         // if (performance.now() - this.lastFire > 70)
         this.fired = this.fire(timeStamp, timeS);
         // if (this.fired) {
         //     console.log(`timeStamp: ${timeStamp}, frameTime: ${actionTime}`);
         // }
     }
+
     addRollBackAction(timeStamp) {
         // let action = {
         //     startPosition: this.oldPosition,
@@ -166,6 +229,7 @@ export class Player extends THREE.Object3D {
         // }
         // this.actions.set(timeStamp, action);
     }
+
     addAction(timeStamp) {
         // let action = {
         //     startPosition: this.oldPosition.clone(),
@@ -192,6 +256,7 @@ export class Player extends THREE.Object3D {
         //     this.controls.sendActionToPeer(data);
         // }
     }
+
     findStateAtTime(time) {
         // let iterator = this.actions.entries();
         // let action = iterator.next().value;
@@ -211,6 +276,7 @@ export class Player extends THREE.Object3D {
         // }
         // // if (previousAction)// later
         // //     action = previousAction;
+
         // if (!action)
         //     return;
         // // this.position.copy(action[1].startPosition).addScaledVector(action[1].movementVector, 1);
@@ -227,6 +293,7 @@ export class Player extends THREE.Object3D {
         // // console.log("last fired of action: ", action[1].lastFire, " and of prev action: ", previousAction[1].lastFire);
         // this.actions.clear();
     }
+
     findPositionAtTime(time) {
         // let iterator = this.actions.entries();
         // let action = iterator.next().value;
@@ -247,6 +314,7 @@ export class Player extends THREE.Object3D {
         // this.movementVector.copy(previousAction[1].movementVector);
         // this.position.copy(this.oldPosition).addScaledVector(this.movementVector, 1);
     }
+
     getCurrentPosition(radius) {
         let vect = new THREE.Vector3();
         vect.copy(this.movementVector).normalize();
@@ -255,17 +323,20 @@ export class Player extends THREE.Object3D {
         point.addScaledVector(this.movementVector, radius);
         return point;
     }
+
     getOldPosition(radius, s) {
         let vect = new THREE.Vector3();
         vect.copy(this.movementVector).normalize();
         let point = new THREE.Vector3();
         point.copy(this.oldPosition);
-        point.addScaledVector(this.movementVector, -radius);
+        point.addScaledVector(this.movementVector, - radius);
         return point;
     }
+
     lastPosition(s) {
         return this.oldPosition;
     }
+
     fire(time, span) {
         if (!this.inputs.fire()) {
             if (this.cannon.head.position.z < -2)
@@ -274,9 +345,10 @@ export class Player extends THREE.Object3D {
         }
         if (this.cannon.head.position.z > -2.4)
             this.cannon.head.position.z -= 0.1;
+
         let start = this.lastFire + this.fireRate;
         if (start < time) {
-            console.log(`start: ${start} < time: ${time}`);
+            console.log(`start: ${start} < time: ${time}`);   
             start = time;
         }
         // console.log(`start: ${start}, time: ${time}`)
@@ -290,46 +362,57 @@ export class Player extends THREE.Object3D {
             let spawnTime = this.lastFire - this.bulletDelay;
             console.log('fired at : ', this.lastFire);
             // this.bulletManager.spawnBullet(0xffffff, this.position, this.inputs.direction.clone(), this.inputs.angle, spawnTime);
+            
             // uncomment for bullet sound
             // this.bulletSound.stop();
             // this.bulletSound.setDetune((0.5 - Math.random()) * 50)
             // this.bulletSound.play();
+            
+            
             // console.log('fire rate: ', this.fireRate, `start: ${start}, end: ${end}, timeS: ${spanS}`);
             // console.log("bullet nbr ", this.bulletManager.bullets.size);
             start += this.fireRate;
         }
         return true;
+
     }
 }
-function createCore() {
+
+
+
+function createCore(): THREE.Mesh {
     let scale = 1;
-    const geometry = new THREE.CapsuleGeometry(scale, 1, 4, 6);
-    const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.3, emissive: 0x777777, emissiveIntensity: 0.8 });
-    const core = new THREE.Mesh(geometry, material);
+    const geometry = new THREE.CapsuleGeometry( scale, 1, 4, 6 )
+    const material = new THREE.MeshStandardMaterial( {color: 0xffffff, metalness: 0.3, emissive: 0x777777, emissiveIntensity: 0.8} );
+    const core = new THREE.Mesh( geometry, material );
     core.name = 'core';
     core.position.set(0, 0, 0);
     return core;
 }
+
 class CannonObject extends THREE.Object3D {
+    head: THREE.Mesh;
     constructor() {
         super();
         const invisibleCore = createCore();
         invisibleCore.visible = false;
-        const geometry = new THREE.CylinderGeometry(0, 0.7, 1.5, 6);
-        const material = new THREE.MeshStandardMaterial({ color: 0xffffff, metalness: 0.1, emissive: 0xeeeef7, emissiveIntensity: 0.8 });
-        const cannonHead = new THREE.Mesh(geometry, material);
+
+        const geometry = new THREE.CylinderGeometry( 0, 0.7, 1.5, 6 );
+        const material = new THREE.MeshStandardMaterial( {color: 0xffffff, metalness: 0.1, emissive: 0xeeeef7, emissiveIntensity: 0.8} );
+        const cannonHead = new THREE.Mesh( geometry, material );
         cannonHead.name = 'cannonHead';
         cannonHead.position.y = 0;
         cannonHead.position.z = -2;
         cannonHead.position.x = 0;
-        cannonHead.rotateX(-Math.PI / 2);
+        cannonHead.rotateX(- Math.PI / 2);
         this.head = cannonHead;
         this.add(invisibleCore);
         this.add(cannonHead);
         this.name = 'cannon';
     }
 }
-function createCannon() {
+
+function createCannon(): CannonObject {
     const cannon = new CannonObject();
     return cannon;
 }
--- backend/pong/shooter/static/osnierTS/main.ts
+++ backend/pong/shooter/static/osnierTS/main.ts
@@ -2,34 +2,50 @@ import * as THREE from 'three';
 import { initPlane, Player } from './assets';
 import { KeyControls, getCameraDir } from './keyControls';
 import { gameClock } from './gclock';
-function initThreeJS() {
+
+function initThreeJS(): { scene: THREE.Scene,
+        camera: THREE.PerspectiveCamera,
+        renderer: THREE.WebGLRenderer } {
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
     camera.position.set(0, 150, 0);
-    camera.lookAt(0, 0, 0);
+    camera.lookAt(0,0,0);
+
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio * 1);
-    document.body.appendChild(renderer.domElement);
-    const light = new THREE.AmbientLight(0xffffff);
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setPixelRatio( window.devicePixelRatio * 1);
+
+    document.body.appendChild( renderer.domElement );
+
+    const light = new THREE.AmbientLight( 0xffffff );
     light.intensity = 0.5;
-    scene.add(light);
-    const light2 = new THREE.DirectionalLight(0xffffff, 10);
+    scene.add( light );
+
+    const light2 = new THREE.DirectionalLight( 0xffffff, 10);
     light2.position.set(10, 10, 10);
     light2.lookAt(0, 0, 0);
-    scene.add(light2);
+    scene.add( light2 );
+
     return { scene, camera, renderer };
 }
+
 const { scene, camera, renderer } = initThreeJS();
 const keyControls = new KeyControls(camera);
 const gClock = new gameClock(scene, camera, renderer);
+
 const plane = initPlane();
 scene.add(plane);
+
 const planeFacingVector = getCameraDir(camera);
-const player = new Player(scene);
+
+const player = new Player(new THREE.Vector3(0, 0, 0));
 player.setPlaneVector(planeFacingVector);
 player.addToScene(scene);
-var rollBack = (startTime) => { };
+plane.add(camera);
+
+var rollBack = (startTime) => {}
+
 var animate = (s, timeStamp) => {
     let startTime = gClock.startTime;
     // if (justRolledBack) {
@@ -40,20 +56,26 @@ var animate = (s, timeStamp) => {
     //     foe.update(span, planeFacingVector, lastFrame, lastFrame);
     //     // console.log('(just rolled back) before animete at time', timeStamp, ' postion: ', foe.position);
     // }
+
     player.update(s, timeStamp, timeStamp);
     // foe.update(s, planeFacingVector, timeStamp, timeStamp);
+
     // if (justRolledBack) {
     //     // console.log('(just rolled back) after animete at time', timeStamp + s, ' postion: ', foe.position);
     //     justRolledBack = false;
     // }
+
     // turretBulletManager.update(timeStamp);
     // playerBulletManager.update(timeStamp);
     // foeBulletManager.update(timeStamp);
+
     // if (limit > 5) {
     //     limit = 0;
     //     scale = 1;
     // }
+
     // let beat = musicSyncer.findCurrentBeat();
+
     // let listen = true;
     // if (listen && beat && beat.type && !beat.handled) {
     //     beat.handled = true;
@@ -62,6 +84,7 @@ var animate = (s, timeStamp) => {
     //     limit = 0;
     //     scale = 1.2;
     //     turret.update(beat.time, startTime);
+
     //     switch (beat.type) {
     //         case 1:
     //             turret.fire(0xfc7703);
@@ -74,27 +97,37 @@ var animate = (s, timeStamp) => {
     //             break;
     //     }
     // }
+
     // turret.scale.set(scale, scale, scale)
+    
     // limit++;
+
     // turretBulletManager.checkCollision(player, s);
     // turretBulletManager.checkCollision(foe, s);
     // playerBulletManager.checkCollision(foe, s);
     // foeBulletManager.checkCollision(player, s);
-};
+}
+
 function handleInputs(s, timeStamp) {
     let playerPosition = player.position;
-    let { angle, direction } = keyControls.findPlayerAngle(playerPosition);
+    let {angle, direction} = keyControls.findPlayerAngle(playerPosition)
     let move = keyControls.findPlayerMove();
     let action = keyControls.findPlayerAction();
-    let inputs = player.inputs.set(move, angle, direction, action, timeStamp);
-}
-;
+
+    let inputs = player.inputs.set(move, angle,
+                    direction, action, timeStamp);
+};
+
 function setPlaneVector(camera, player, foe) {
     const planeFacingVector = getCameraDir(camera);
     player.setPlaneVector(planeFacingVector);
     foe.setPlaneVector(planeFacingVector);
 }
+
 gClock.loop(animate, rollBack, handleInputs);
+
 // const connection = new Connection(keyControls, playerSyncData);
 // connection.connectToServer(friend.id);
 // connection.attatchGameStart(startGame);
+
+
